801d6c012b1c5a150c3dfadd8ed55cdf3e39d5e1
mithi2209 <mithila.sm03@gmail.com>
2025-08-29 19:41:25 +0600

    added alert message functionality

--- script/coinsDeduct.js
+++ script/coinsDeduct.js
@@ -1,1 +1,32 @@
-console.log('coins')
+// Coins Deduction
+
+// get call button
+
+const callButton = document.querySelectorAll('.call-btn');
+
+// for every button
+
+for (const button of callButton){
+    button.addEventListener('click',function(e){
+
+      e.preventDefault();
+
+      // get the parent
+
+      const child = button.parentElement; 
+      const parent = child.parentElement; 
+      const grandparent = parent.parentElement; 
+      const greatGrandparent = grandparent.parentElement; 
+
+      console.log("parentsss",greatGrandparent);
+
+      // get service name and number
+
+      const service = greatGrandparent.querySelector(".service-name").innerText;
+      const number = greatGrandparent.querySelector(".service-hotline").innerText;
+
+      // Alert
+
+      alert(`📞 Calling ${service} at ${number}...`);
+    })
+}
