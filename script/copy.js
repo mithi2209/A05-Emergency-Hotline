f60fb1087203cfbfaf9daabceba13be248e19b7d
mithi2209 <mithila.sm03@gmail.com>
2025-08-30 03:22:56 +0600

    finished challenged part

--- script/copy.js
+++ script/copy.js
@@ -1,1 +1,69 @@
-console.log("copy")
+// get copy btn
+const copyButtons = document.querySelectorAll(".copy-btn");
+
+
+
+
+for (const copyBtn of copyButtons){
+    copyBtn.addEventListener('click',function(e){
+      e.preventDefault();
+
+      // get the parent
+
+      const child = copyBtn.parentElement;
+      const parent = child.parentElement;
+      const grandparent = parent.parentElement;
+      const greatGrandparent = grandparent.parentElement;
+      //   console.log(greatestGrandparent);
+
+      //   get hotline number
+      const hotlineNumber =
+        grandparent.querySelector(".service-hotline").innerText;
+
+      // temporary textarea
+      const tempInput = document.createElement("textarea");
+      tempInput.value = hotlineNumber;
+      document.body.appendChild(tempInput);
+
+      // text select
+      tempInput.select();
+
+      // text copy
+      const success = document.execCommand("copy");
+
+      // remove temp text area
+      document.body.removeChild(tempInput);
+
+      if (success) {
+        alert(`Number ${hotlineNumber} Copied`);
+        const countText = document.body.querySelector(".copy-count ");
+        countText.innerText = parseInt(countText.innerText) + 1;
+
+        const countText2 = document.body.querySelector(".copy-count2");
+        countText2.innerText = parseInt(countText2.innerText) + 1;
+      } else {
+        alert("Failed to Copy!");
+      }
+
+      //   adding to history
+
+      // get time
+      const time = new Date().toLocaleTimeString();
+    //   get history div
+    const historyDiv = document.getElementById("call-history");
+
+    const historyItems = document.createElement("div");
+    historyItems.classList.add(
+      "history-item",
+      "rounded-lg",
+      "my-4",
+      "p-4",
+      "flex",
+      "justify-between",
+      "items-center",
+      "bg-[#FAFAFA]"
+    );
+    historyItems.innerHTML =`<strong>${hotlineNumber}</strong> Called at : ${time}`;
+    historyDiv.appendChild(historyItems);
+    });
+}
--- script/heartCount.js
+++ script/heartCount.js
@@ -3,6 +3,7 @@
 let heartCountNumber = 0;
  
 const totalCountDisplay = document.getElementById("heart-count");
+const totalCountDisplay2 = document.getElementById("heart-count2");
 const heartBtn = document.querySelectorAll(".heart-icon-btn");
 
         for (const number of heartBtn){
@@ -12,6 +13,7 @@ const heartBtn = document.querySelectorAll(".heart-icon-btn");
                    heartCountNumber++;
 
                    totalCountDisplay.innerText = heartCountNumber;
+                   totalCountDisplay2.innerText = heartCountNumber;
 
             });
          
